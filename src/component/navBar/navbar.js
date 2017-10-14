925fe39613ea047b55a5ebb3eda3b0ae34ea54a6
Shiv Raj <rajshiv169@gmail.com>
2017-10-14 13:26:59 +0530

    Navbar edit

--- src/component/navBar/navbar.js
+++ src/component/navBar/navbar.js
@@ -12,30 +12,51 @@ class nav_bar extends Component{
 	render(){
 		return (
       <div>
+
         <nav className="navbar">
-  				  <div className="container-fluid">
-    				<div className="navbar-header">
-            <span className="glyphicon glyphicon-briefcase"></span>
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
               <a className="navbar-brand" href="\">infyJobs</a>
-    				</div>
-    				<ul className="nav navbar-nav">
-      					<li className="active"><a href="#">Home</a></li>
-      					<li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
-      	  			<ul className="dropdown-menu">
-        		  		<li><a href="#">Page 1-1</a></li>
-        		  		<li><a href="#">Page 1-2</a></li>
-          				<li><a href="#">Page 1-3</a></li>
-        			</ul>
-      					</li>
-   				 	  <li><a href="#">Page 2</a></li>
- 				  	 </ul>
-	    			<ul className="nav navbar-nav navbar-right">
-					   <Signup/> 
-          	 <Login/>
-					</ul>
-  					</div>
-				</nav>
-    </div>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#"><i className="fa fa-home" aria-hidden="true"></i>Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">dropdown <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                  </ul>
+                </li>
+              </ul>
+              
+              <form className="navbar-form navbar-left" role="search">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Search" name="q" />
+                  <div className="input-group-btn">
+                    <button className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i></button>
+                  </div>
+                </div>
+              </form>
+
+              <ul className="nav navbar-nav navbar-right">
+                <Signup/> 
+                <Login/>
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+
+      </div>
 		);
 	}
 
--- src/component/navBar/sign_up.js
+++ src/component/navBar/sign_up.js
@@ -37,37 +37,49 @@ class signup extends Component {
 	render() {
 		return (
 			<div>
-			<button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Sign Up</button>
+				
 
-			<div id="myModal" className="modal fade" role="dialog">
- 				<div className="modal-dialog">
-				<div className="modal-content">
-      			<div className="modal-header">
-       			<button type="button" className="close" data-dismiss="modal">&times;</button>
-        <h4 className="modal-title">Sign Up</h4>
-      </div>
-      <div className="modal-body">
-        <form>
-       		<li>Email<input value={this.state.login_e} className="login-user" onChange={this.handleChange_email} />
-       		</li>
-       		<li>First Name<input value={this.state.login_e} className="login-user" onChange={this.handleChange_first} />
-       		</li>
-       		<li>Last Name<input value={this.state.login_e} className="login-user" onChange={this.handleChange_lastName} />
-       		</li>
-       		<li>Nationality<input value={this.state.login_e} className="login-user" onChange={this.handleChange_nation} />
-       		</li>
-       		<li>Password<input value={this.state.login_e} type='password' className="login-user" onChange={this.handleChange_pass} />
-       		</li>
-       		<li>Confirm Password<input value={this.state.login_e} className="login-user" onChange={this.handleChange_cpass} />
-       		</li>
-       	</form>
-      </div>
-      <div className="modal-footer">
-        		<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-     			 </div>
-    			</div>
- 			 </div>
-			</div>
+							
+				<a className="signup-text" data-toggle="modal" data-target="#myModal"><i className="fa fa-user" aria-hidden="true"></i>Log In/Sign Up</a>
+				<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	    			<div className="modal-dialog">
+	        			<div className="modal-content">
+				            <div className="modal-body">
+				                <div role="tabpane1">
+				                	<button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>    
+				                    <ul className="nav nav-tabs" role="tablist">
+				                        <li role="presentation" className="active"><a href="" aria-controls="login" role="tab" data-toggle="tab">log In</a></li>
+				                        <li role="presentation"><a href="#" aria-controls="signup" role="tab" data-toggle="tab">Sign Up</a></li>
+				                    </ul>
+				                    
+				                    <div className="tab-content">
+				                        <div role="tabpanel" className="tab-pane active" id="log-in"></div>
+				                        <div role="tabpane2" className="tab-pane" id="sign-up">
+				                               <form>
+										       		<li>Email<input value={this.state.login_e} className="login-user" onChange={this.handleChange_email} />
+										       		</li>
+										       		<li>First Name<input value={this.state.login_e} className="login-user" onChange={this.handleChange_first} />
+										       		</li>
+										       		<li>Last Name<input value={this.state.login_e} className="login-user" onChange={this.handleChange_lastName} />
+										       		</li>
+										       		<li>Nationality<input value={this.state.login_e} className="login-user" onChange={this.handleChange_nation} />
+										       		</li>
+										       		<li>Password<input value={this.state.login_e} type='password' className="login-user" onChange={this.handleChange_pass} />
+										       		</li>
+										       		<li>Confirm Password<input value={this.state.login_e} className="login-user" onChange={this.handleChange_cpass} />
+										       		</li>
+										       	</form>
+				                        </div>
+				                    </div>
+				                </div>
+				            </div>
+				            <div className="modal-footer">
+				                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+				                <button type="button" className="btn btn-primary">Signup</button>
+				            </div>
+				        </div>
+				    </div>
+				</div>
 			</div>
 		);
 	}
